d0bc9c44cbcd5732b01ec80556402a17835f2300
Ivy Sylvia <ivysylvia6@gmail.com>
2018-10-23 17:55:06 +0300

    added db

--- db/ivyproject.sql
+++ db/ivyproject.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.6.4
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Oct 23, 2018 at 02:39 PM
--- Server version: 5.7.19
--- PHP Version: 5.6.31
+-- Host: 127.0.0.1
+-- Generation Time: Oct 23, 2018 at 02:52 PM
+-- Server version: 5.7.14
+-- PHP Version: 5.6.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -28,9 +26,8 @@ SET time_zone = "+00:00";
 -- Table structure for table `shopperinfo`
 --
 
-DROP TABLE IF EXISTS `shopperinfo`;
-CREATE TABLE IF NOT EXISTS `shopperinfo` (
-  `shopperID` int(20) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `shopperinfo` (
+  `shopperID` int(20) NOT NULL,
   `shopperfName` varchar(40) NOT NULL,
   `shoppersName` varchar(40) NOT NULL,
   `shopperusername` varchar(40) NOT NULL,
@@ -38,9 +35,8 @@ CREATE TABLE IF NOT EXISTS `shopperinfo` (
   `shopperPhonenumber` varchar(15) NOT NULL,
   `shopperCounty` varchar(40) NOT NULL,
   `shopperConstituency` varchar(40) NOT NULL,
-  `shopperPassword` varchar(40) NOT NULL,
-  PRIMARY KEY (`shopperID`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+  `shopperPassword` varchar(40) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `shopperinfo`
@@ -48,8 +44,26 @@ CREATE TABLE IF NOT EXISTS `shopperinfo` (
 
 INSERT INTO `shopperinfo` (`shopperID`, `shopperfName`, `shoppersName`, `shopperusername`, `shopperemail`, `shopperPhonenumber`, `shopperCounty`, `shopperConstituency`, `shopperPassword`) VALUES
 (6, ':pkjhgjv', 'oiuhyugy', 'pijhu', 'jobokello5@gmail.com', '567987654', 'nairobi', 'Embakasi', '9a102318a80b87454b1f1b33bf5223c8');
-COMMIT;
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `shopperinfo`
+--
+ALTER TABLE `shopperinfo`
+  ADD PRIMARY KEY (`shopperID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `shopperinfo`
+--
+ALTER TABLE `shopperinfo`
+  MODIFY `shopperID` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
