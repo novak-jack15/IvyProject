1d85e91e75ec9b2d2c6f9c71cca3e5d4643e4cb6
jobokello <jobokello5@gmail.com>
2018-10-30 18:04:00 +0300

    login working

--- html/trpAgentHome.php
+++ html/trpAgentHome.php
@@ -2,6 +2,88 @@
 	session_start();
 	echo 'time to deliver';
 	echo $_SESSION['trpAgentID'];
-    echo $_SESSION['trpAgentUsername'];
+    $login_user = $_SESSION['trpAgentUsername'];
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>Shopping Page</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap3/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/home.css" rel="stylesheet" type="text/css">
+      <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Ivy Designs</a>
+        </div>
+
+        <!--links for navbar on the left-->
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="#">Home</a></li>
+              <li><a href="#">Trends</a></li>
+              <li><a href="#">Men</a></li>
+              <li><a href="#">Women</a></li>
+              <li><a href="#">Kids</a></li>
+              <li><a href="#">Couples</a></li>
+            </ul>
+
+            <!--links for navbar on the right-->
+            <ul class="nav navbar-nav navbar-right">
+              <li><a title = "click to views your profile" href="#"><span class="glyphicon glyphicon-user"></span><span class="userloggedin"><strong> <?php echo $login_user;?></strong></span><span ></span></a></li>
+              <li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout<span></span></a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      	<footer class=" site-footer navbar navbar-fixed-bottom navbar-default container-fluid">
+		    <div id="theContent">
+		        <div class="col-md-3 col-sm-3">
+
+		        </div>
+		        <div class="gridInfo col-md-6 col-sm-6">
+		            <h5>Help & support</h5>
+		            <ul >
+		                <li><a href="#">Home</a></li>
+		                <li><a href="#">About Us</a></li>
+		                <li><a href="#">Contact US</a></li>
+		            </ul>
+		        </div>
+
+		        <div class="gridInfo col-md-3 col-sm-3">
+		            <h5>Social Media</h5>
+		            <ul>
+						<li><img src="../icons/Facebook_32.png"></span></li>
+						<li><img src="../icons/Twitter_32.png"></span></li>
+					<ul>
+		        </div>
+		    </div>
+		</footer>
+</div>
+
+</body>
+</html>
